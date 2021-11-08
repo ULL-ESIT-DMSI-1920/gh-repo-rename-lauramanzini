79a4425010b63d3125ebb95dd49ac134db19f07f
lauramanzini <232566@studenti.unimore.it>
2021-11-08 11:44:00 +0000

    commit command

--- gh-repo-view.js
+++ gh-repo-view.js
@@ -7,6 +7,22 @@ const deb = (...args) => {
 const fs = require("fs");
 const shell = require('shelljs'); 
 const { Command } = require('commander');
+program.version('0.0.1');
 
+program
+  .option('-r, --repo <type>', 'output extra debugging')
+  .option('-o, --org <type>', 'small pizza size')
+  .option('--help', 'help for more informations')
+  .option('-V', 'flavour of pizza');
+
+program.parse(process.argv);
+
+const options = program.opts();
+if (options.debug) console.log(options);
+console.log('pizza details:');
+if (options.small) console.log('- small pizza size');
+if (options.pizzaType) console.log(`- ${options.pizzaType}`);
 
 console.log("It is working")
+
+
