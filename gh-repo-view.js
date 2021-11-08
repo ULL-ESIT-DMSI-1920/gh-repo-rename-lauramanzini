2f8d2534f7b2700354f0239bfce02f57504bd5d6
lauramanzini <232566@studenti.unimore.it>
2021-11-08 11:56:58 +0000

    en-clase

--- gh-repo-view.js
+++ gh-repo-view.js
@@ -6,22 +6,20 @@ const deb = (...args) => {
 
 const fs = require("fs");
 const shell = require('shelljs'); 
-const { Command } = require('commander');
-program.version('0.0.1');
+const { program } = require('commander');
 
 program
+  .version('0.1.1')
   .option('-r, --repo <type>', 'output extra debugging')
   .option('-o, --org <type>', 'small pizza size')
-  .option('--help', 'help for more informations')
-  .option('-V', 'flavour of pizza');
+ 
 
 program.parse(process.argv);
 
 const options = program.opts();
-if (options.debug) console.log(options);
-console.log('pizza details:');
-if (options.small) console.log('- small pizza size');
-if (options.pizzaType) console.log(`- ${options.pizzaType}`);
+if (options.repo) console.log(options.repo);
+
+if (options.org) console.log(options.org);
 
 console.log("It is working")
 
