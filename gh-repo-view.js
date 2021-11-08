f8723b6e8499cf3f7cdca33c67b853e14347b467
lauramanzini <232566@studenti.unimore.it>
2021-11-08 12:26:16 +0000

    finl-de-la-clase

--- gh-repo-view.js
+++ gh-repo-view.js
@@ -1,26 +1,42 @@
 #! librerias que son muy util
 const ins = require("util").inspect;
-const deb = (...args) => { 
-    if (debug) console.log(ins(...args, {depth: null})); 
+const deb = (...args) => {
+  console.log(ins(...args, { depth: null }));
 };
 
 const fs = require("fs");
-const shell = require('shelljs'); 
+const shell = require('shelljs');
 const { program } = require('commander');
 
 program
   .version('0.1.1')
   .option('-r, --repo <type>', 'output extra debugging')
   .option('-o, --org <type>', 'small pizza size')
- 
 
 program.parse(process.argv);
 
 const options = program.opts();
-if (options.repo) console.log(options.repo);
 
+
+if (options.repo) console.log(options.repo);
 if (options.org) console.log(options.org);
 
+console.log(`program.args = ${deb(program.args)}`)
 console.log("It is working")
 
+// comprobar que git y gh están instalados
+if (!shell.which('git')) {
+  shell.echo('Sorry, this script requires git');
+  shell.exit(1);
+}
 
+if (!shell.which('gh')) {
+  shell.echo('Sorry, this script requires gh cli');
+  shell.exit(1);
+}
+
+const org = options.org;
+if (!org) {
+  if (program.args.length < 2) program.help()
+
+}
