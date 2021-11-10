a0f3d3eb0ab12fe45bc4817239ebb675bc31bef3
lauramanzini <232566@studenti.unimore.it>
2021-11-10 12:36:00 +0000

    error solution not solved

--- gh-repo-view.js
+++ gh-repo-view.js
@@ -21,8 +21,8 @@ program
 
 program.parse(process.argv);
 
+//let args = program.args;
 const options = program.opts();
-
 
 if (options.repo) console.log(options.repo);
 if (options.org) console.log(options.org);
@@ -41,8 +41,26 @@ if (!shell.which('gh')) {
   shell.exit(1);
 }
 
-const {org, repo }  = options;
-if (!org || !repo) {
-  if (args.length < 2) program.help()
+let newName;
+if (!org) {
+  [org, repo] = args[0].split("/");
+  console.log(`org and repo ${org} ${repo}`);
+  console.log(`newName = ${newName}`)
 }
-if (org && repo && args.length < 1)  program.help();
+if(!newName) newName = args[0]
+if(!org || !repo || newName) program.help()
+
+/*
+if (!repo) {
+  [org, repo] = args[1].split("/")
+}
+
+if ( args.length < 1) program.help();
+if (!org || !repo) {
+  program.help();
+}
+else if (args.length === 0) {
+  program.help();
+}
+console.log('The number of arguments is correct')
+*/
